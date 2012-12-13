95b8e63bd93ac7e81a7866d51fa0baa188e8c596
Jernej Virag <jernej@virag.si>
2012-12-13 10:54:03 +0100

    SAP muxer session name fix

--- libavformat/sapenc.c
+++ libavformat/sapenc.c
@@ -158,6 +158,11 @@ static int sap_write_header(AVFormatContext *s)
         av_strlcpy(contexts[i]->filename, url, sizeof(contexts[i]->filename));
     }
 
+    AVDictionaryEntry* title = av_dict_get(s->metadata, "title", NULL, 0);
+    if (s->nb_streams > 0 && title) {
+      av_dict_set(&contexts[0]->metadata, "title", title->value, 0);
+    }
+
     ff_url_join(url, sizeof(url), "udp", NULL, announce_addr, port,
                 "?ttl=%d&connect=1", ttl);
     ret = ffurl_open(&sap->ann_fd, url, AVIO_FLAG_WRITE,
